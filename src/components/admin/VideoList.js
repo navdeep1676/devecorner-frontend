34be2125d2e1d135b2cc44877a813e9ddec5b9c1
Navdeep <navdeepdahiya753@gmail.com>
2023-03-12 10:33:18 +0530

    updates

--- src/components/admin/VideoList.js
+++ src/components/admin/VideoList.js
@@ -1,11 +1,72 @@
-import React from "react";
-import { CustomTable } from "./comman/CustomTable";
-
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../../helpers/app-context";
+import { Button, Table } from "antd";
+import { Link } from "react-router-dom";
+import { FaRegEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+import { GrView } from "react-icons/gr";
+const columns = [
+  {
+    title: "SNo",
+    dataIndex: "key",
+  },
+  {
+    title: "Title",
+    dataIndex: "title",
+  },
+  {
+    title: "Description",
+    dataIndex: "desc",
+  },
+  {
+    title: "Url",
+    dataIndex: "url",
+  },
+  {
+    title: "Action",
+    dataIndex: "action",
+  },
+];
 export const VideoList = () => {
+  const ctx = useContext(AppContext);
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+  const getVideos = async () => {
+    const result = await ctx.HttpGet("video");
+    if (result.status === true) {
+      setVideos(result.data);
+    }
+  };
+  const data = [];
+  for (let i = 0; i < videos.length; i++) {
+    data.push({
+      key: i + 1,
+      title: videos[i].title.substr(0, 70),
+      desc: videos[i].desc.substr(0, 70),
+      url: (
+        <a className="d-block" target={"_blank"} href={videos[i].url}>
+          <GrView className="text-danger fs-3" />
+        </a>
+      ),
+      action: (
+        <>
+          <Link to={"/admin/video/1"} className="mx-3">
+            <FaRegEdit className="text-danger fs-3" />
+          </Link>
+          <Button className="mx-3 border border-0">
+            <AiFillDelete className="text-danger fs-3" />
+          </Button>
+        </>
+      ),
+    });
+  }
   return (
     <div>
       <h5>Videos</h5>
-      <CustomTable />
+      <Table columns={columns} dataSource={data} />
     </div>
   );
 };
--- src/components/comman/CustomCard.js
+++ src/components/comman/CustomCard.js
@@ -6,12 +6,12 @@ export const CustomCard = (props) => {
     <>
       {data?.map((item, index) => {
         return (
-          <div key={index} className="col-12 col-sm-6 col-lg-3 mt-4 mt-lg-0">
+          <div key={index} className="col-12 col-sm-6 col-lg-4 mt-4 mt-lg-4">
             <Card style={{ width: "100%" }}>
               <Card.Img variant="top" src={item.image.url} />
               <Card.Body>
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Text>{item.desc}</Card.Text>
+                <Card.Title>{item.title.substr(0, 49) + "..."}</Card.Title>
+                <Card.Text>{item.desc.substr(0, 140) + "..."}</Card.Text>
                 <a className="button mt-4" href={item.link} target={"_blank"}>
                   Explore Now
                 </a>
