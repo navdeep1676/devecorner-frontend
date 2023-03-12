a8dabbd3c67b43b82be25e9eb45c76b405df3b7c
Navdeep <navdeepdahiya753@gmail.com>
2023-03-12 12:53:29 +0530

    updates

--- src/components/VideoDeatils.js
+++ src/components/VideoDeatils.js
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { AppContext } from "../helpers/app-context";
 
 export const VideoDeatils = () => {
-  return <div>VideoDeatils</div>;
+  const ctx = useContext(AppContext);
+  const location = useLocation();
+  const [videoData, setVideoData] = useState([]);
+  const getVideoSlug = location.pathname.split("/")[2];
+  useEffect(() => {
+    getVideoDetails();
+  }, []);
+  const getVideoDetails = async () => {
+    const result = await ctx.HttpGet("video/" + getVideoSlug);
+    if (result.status === true) {
+      setVideoData(result.data);
+    }
+  };
+  return (
+    <section className="contact-wrapper-1 py-5 pt-0">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-12">
+            <h1 className="heading mb-4 text-center">
+              <span style={{ color: "#000" }}>{videoData?.title} </span>
+            </h1>
+          </div>
+        </div>
+        <div className="row pt-4">
+          <div className="col-12">
+            <img
+              src={videoData?.image?.url}
+              alt={videoData?.slug}
+              className="img-fluid"
+            />
+            <div
+              className="mt-5"
+              dangerouslySetInnerHTML={{ __html: videoData?.summary }}
+            ></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
--- src/components/comman/CustomCard.js
+++ src/components/comman/CustomCard.js
@@ -1,25 +1,39 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 export const CustomCard = (props) => {
   const { data, type } = props;
+  const navigate = useNavigate();
+  const goToPage = (slug) => {
+    navigate(`/${type}/${slug}`);
+  };
   return (
     <>
       {data?.map((item, index) => {
         return (
-          <Link
+          <div
             key={index}
-            to={`/admin/video/${item.slug}`}
             className="d-block col-12 col-sm-6 col-lg-4 mt-4 mt-lg-4"
           >
             <Card style={{ width: "100%" }}>
-              <Card.Img variant="top" src={item.image.url} />
+              <Card.Img
+                onClick={() => goToPage(item?.slug)}
+                variant="top"
+                className="cursor-pointer"
+                src={item.image.url}
+              />
               <Card.Body>
-                <Card.Title className="text-decoration-none">
+                <Card.Title
+                  onClick={() => goToPage(item?.slug)}
+                  className="text-decoration-none cursor-pointer"
+                >
                   {" "}
                   {item.title.substr(0, 49) + "..."}
                 </Card.Title>
-                <Card.Text className="text-decoration-none">
+                <Card.Text
+                  onClick={() => goToPage(item?.slug)}
+                  className="text-decoration-none cursor-pointer"
+                >
                   {item.desc.substr(0, 140) + "..."}
                 </Card.Text>
                 <a className="button mt-4" href={item.url} target={"_blank"}>
@@ -27,7 +41,7 @@ export const CustomCard = (props) => {
                 </a>
               </Card.Body>
             </Card>
-          </Link>
+          </div>
         );
       })}
     </>
