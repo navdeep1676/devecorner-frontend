f5dbf2b2101ec844d62575ef0a5a47ea1e397931
Navdeep <navdeepdahiya753@gmail.com>
2023-03-12 13:20:44 +0530

    updates

--- src/components/comman/CustomCard.js
+++ src/components/comman/CustomCard.js
@@ -4,36 +4,28 @@ import { Link, useNavigate } from "react-router-dom";
 export const CustomCard = (props) => {
   const { data, type } = props;
   const navigate = useNavigate();
-  const goToPage = (slug) => {
-    navigate(`/${type}/${slug}`);
-  };
+
   return (
     <>
       {data?.map((item, index) => {
         return (
-          <div
+          <Link
+            to={`/${type}/${item?.slug}`}
             key={index}
             className="d-block col-12 col-sm-6 col-lg-4 mt-4 mt-lg-4"
           >
             <Card style={{ width: "100%" }}>
               <Card.Img
-                onClick={() => goToPage(item?.slug)}
                 variant="top"
                 className="cursor-pointer"
                 src={item?.image?.url}
               />
               <Card.Body>
-                <Card.Title
-                  onClick={() => goToPage(item?.slug)}
-                  className="text-decoration-none cursor-pointer"
-                >
+                <Card.Title className="text-decoration-none cursor-pointer">
                   {" "}
                   {item?.title.substr(0, 49) + "..."}
                 </Card.Title>
-                <Card.Text
-                  onClick={() => goToPage(item?.slug)}
-                  className="text-decoration-none cursor-pointer"
-                >
+                <Card.Text className="text-decoration-none cursor-pointer">
                   {item?.desc.substr(0, 140) + "..."}
                 </Card.Text>
                 <a className="button mt-4" href={item?.url} target={"_blank"}>
@@ -41,7 +33,7 @@ export const CustomCard = (props) => {
                 </a>
               </Card.Body>
             </Card>
-          </div>
+          </Link>
         );
       })}
     </>
